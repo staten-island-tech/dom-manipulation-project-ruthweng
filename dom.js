c88875d9636d2ae171fc9b34907f56d228cfe33c
ruthweng <ruthweng333@gmail.com>
2023-10-24 13:14:10 -0400

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,5 +1,13 @@
+/*
+-Create an object called “DOMSelectors” to hold your DOM Selectors
+-Create a function that creates an object and calls the following functions
+-Create a function that injects the newly created object into the DOM
+-Create a function that clears the input fields after injecting the object
+-Create a function to remove an object after they have been created
+*/
+
 const form = document.querySelector("#form");
-const firstName = document.querySelector(".first-name");
+const firstName = document.querySelector(".first- name");
 console.log(firstName);
 DOMSelectors = {
     form: document.querySelector("#form"),
