98861f22f4921382f0ed43424384f433dd3ec573
ruthweng <ruthweng333@gmail.com>
2023-11-08 02:26:37 -0500

    oh

--- dom.js
+++ dom.js
@@ -6,18 +6,45 @@
 -Create a function to remove an object after they have been created
 */
 
-const form = document.querySelector("#form");
-const firstName = document.querySelector(".first- name");
-console.log(firstName);
-DOMSelectors = {
+const DOMSelectors = {
     form: document.querySelector("#form"),
-    firstName: document.querySelector(".first-name"),
-    h2s: document.querySelectorAll("h2"),
+    titleName: document.getElementById("title-name"),
+    captionText: document.getElementById("caption-text"),
+    imageLink: document.getElementById("image-link"),
+    box: document.querySelector("#box"),
+    card: document.querySelector(".card"),
+    button: document.getElementById("btn"),
+   
 };
+    
+
+function addcard(){
+    DOMSelectors.box.insertAdjacentHTML("afterend",
+        `<div class="card" id="box">
+        <h1 class="title-name"> ${DOMSelectors.titleName.value} </h1>
+        <img src= "${DOMSelectors.imageLink.value}" alt="image" class="card-image">
+        <h3 class="caption-text"> ${DOMSelectors.captionText.value} </h3>
+        <button class="remove"> Remove </button>
+        </div>`);
+}
+
+function removecard(){
+    document.querySelectorAll(".remove").forEach((button) => {
+       button.addEventListener("click", function(event){
+           event.target.parentElement.remove();
+       });
+    });
+}
+
+function clearFields() {
+   DOMSelectors.titleName.value ="";
+   DOMSelectors.imageLink.value="";
+   DOMSelectors.captionText.value="";
+}
 
 DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
-    DOMSelectors.h2s.forEach(
-        (header) => (header.textContent = DOMSelectors.firstName.value)
-    );
-});
+    addcard();
+    removecard();
+    clearFields();
+    });
